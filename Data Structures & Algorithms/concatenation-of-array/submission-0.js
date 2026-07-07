dae32de166f158df230b5eeb13938cc98013f839
Kristen <63243258+kriistenn@users.noreply.github.com>
2026-07-07 14:06:30 +0700

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-0.js
@@ -0,0 +1,12 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    getConcatenation(nums) {
+       let arr = [...nums, ...nums]
+
+       return arr
+    }
+}
+
